7fda3e95c0cd49c894f720bf3c127c7f164b91fb
Becky Hamm <becky@metricslice.com>
2015-02-12 18:14:41 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+$(function(){
+
+  var getData = function(keywords, callback) {
+    var params = {
+      api_key: "niqoyrl7dver15xzb6mp2c7e",
+      includes: "Images,Shop"
+    };
+
+    if (!callback && typeof keywords === "function") {
+      callback = keywords;
+      keywords = null;
+    }
+
+    if (keywords && keywords.length) {
+      params.keywords = keywords;
+    }
+
+    $.ajax("https://openapi.etsy.com/v2/listings/active.js", {
+      data: params,
+      dataType: "jsonp",
+      success: callback
+    });
+  };
+
+});
